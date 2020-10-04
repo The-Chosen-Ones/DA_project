ef7bffe9429271ba227ed44cb01a7c4b564dc785
Vedansh Mittal <56213441+VedanshM@users.noreply.github.com>
2020-10-04 18:40:11 +0530

    slight refactor in reads

--- code/reads.py
+++ code/reads.py
@@ -2,83 +2,84 @@ import pymysql
 from db_description import *
 
 
-def get_account_details(con: pymysql.connections.Connection, email: str, DEBUG=False):
-    query = "SELECT * FROM ACCOUNT WHERE Email_id = {}".format(email)
+def general_get_details(con: pymysql.connections.Connection, cols: str, table: str, cond: str, DEBUG=False):
+    #  return list of dicts
+    query = f"SELECT {cols} FROM {table} WHERE {cond}"
     if DEBUG:
         print(query)
     with con.cursor() as cur:
         try:
             cur.execute(query)
             if cur.rowcount:
-                return cur.fetchone()
+                return list(cur.fetchall())
+            else:
+                return []
         except Exception as e:
             con.rollback()
-            print("Failed to get acc details")
+            print(f"Failed to get {table} details")
             print(">>>>>>>>>>>>>", e)
             return ERROR_CODE
-    return ERROR_CODE
+
+
+def get_account_details(con: pymysql.connections.Connection, email: str, DEBUG=False):
+    ret = general_get_details(
+        con, "*", "ACCOUNT", f"Email_id = {email}", DEBUG)
+    if ret == ERROR_CODE or ret == []:
+        return ERROR_CODE
+    return ret[0]
 
 
 def get_student_details(con: pymysql.connections.Connection, email: str, DEBUG=False):
-    query = "SELECT * FROM STUDENT WHERE Email_id = {}".format(email)
-    if DEBUG:
-        print(query)
-    st_row = {}
-    with con.cursor() as cur:
-        try:
-            cur.execute(query)
-            if not cur.rowcount:
-                return ERROR_CODE
-            else:
-                st_row = cur.fetchone()
-        except Exception as e:
-            con.rollback()
-            print("Failed to get student details")
-            print(">>>>>>>>>>>>>", e)
-            return ERROR_CODE
 
-    acc_row = get_account_details(con, email, DEBUG)
-    if acc_row == ERROR_CODE:
+    ret = general_get_details(
+        con, "*", "STUDENT", "Email_id = {}".format(email), DEBUG)
+    if ret == ERROR_CODE or ret == []:
         return ERROR_CODE
     row = {}
-    row.update(st_row)
-    row.update(acc_row)
+    row.update(ret[0])
+
+    ret = get_account_details(con, email, DEBUG)
+    if ret == ERROR_CODE:
+        return ERROR_CODE
+    row.update(ret)
+
     return row
 
 
 def get_instructor_details(con: pymysql.connections.Connection, email: str, DEBUG=False):
-    query = "SELECT * FROM INSTRUCTOR WHERE Email_id = {}".format(email)
-    if DEBUG:
-        print(query)
-    inst_row = {}
-    with con.cursor() as cur:
-        try:
-            cur.execute(query)
-            if cur.rowcount:
-                inst_row = cur.fetchone()
-            else:
-                return ERROR_CODE
-        except Exception as e:
-            con.rollback()
-            print("Failed to get instructor details")
-            print(">>>>>>>>>>>>>", e)
-            return ERROR_CODE
 
-    acc_row = get_account_details(con, email, DEBUG)
-    if acc_row == ERROR_CODE:
+    ret = general_get_details(con, "*", "INSTRUCTOR",
+                              "Email_id = {}".format(email), DEBUG)
+    if ret == ERROR_CODE or ret == []:
         return ERROR_CODE
     row = {}
-    row.update(inst_row)
-    row.update(acc_row)
-    query = f"SELECT Degree FROM DEGREE WHERE Email_id = {email}"
-    row["DEGREE"] = []
-    if DEBUG:
-        print(query)
-    with con.cursor() as cur:
-        try:
-            cur.execute(query)
-            if cur.rowcount:
-                row["DEGREE"] = list(cur.fetchall().values())
-        except Exception as e:
-            print(">>>>>>>>>>>>>", e)
+    row.update(ret[0])
+
+    ret = get_account_details(con, email, DEBUG)
+    if ret == ERROR_CODE:
+        return ERROR_CODE
+    row.update(ret)
+
+    ret = general_get_details(con, "Degree", "DEGREE",
+                              "Email_id = {}".format(email))
+    if ret == ERROR_CODE:
+        return ERROR_CODE
+    row["Degree"] = [row["Degree"] for row in ret]
     return row
+
+
+def get_team_details(con: pymysql.connections.Connection, tname: str, DEBUG=False):
+    ret = general_get_details(
+        con, "*", "TEAM", "Team_name = {}".format(tname), DEBUG)
+    if ret == ERROR_CODE or ret == []:
+        return ERROR_CODE
+    row = {}
+    row.update(ret[0])
+
+    ret = general_get_details(
+        con, "Textbook", "TEXTBOOK", "Team_name = {}".format(tname), DEBUG)
+    if ret == ERROR_CODE:
+        return ERROR_CODE
+    row["Textbook"] = [row["Textbook"] for row in ret]
+    return row
+
