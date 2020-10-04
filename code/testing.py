0f9bc92fbfe3fa35406e5b51706f0b27cd386bb8
Vedansh Mittal <56213441+VedanshM@users.noreply.github.com>
2020-10-04 22:18:53 +0530

    bug fix

--- code/testing.py
+++ code/testing.py
@@ -9,16 +9,24 @@ con = pymysql.connect(host='127.0.0.1',
                       port=5005,
                       cursorclass=pymysql.cursors.DictCursor)
 
+# row = {
+#     "Email_id": "prof@mail.com",
+#     "First_name": "pro",
+#     "Family_name": "f",
+#     "Password": "password",
+#     "Mobile": "1023456789",
+#     "Sex": "M",
+#     "Address": ["lab 332323"],
+#     "Degree": ["mtech", "btech", "phd"],
+#     "Sup_id": "prof@mail.com"
+# }
+
 row = {
-    "Email_id": "prof@mail.com",
-    "First_name": "pro",
-    "Family_name": "f",
-    "Password": "password",
-    "Mobile": "1023456789",
-    "Sex": "M",
-    "Address": ["lab 332323"],
-    "Degree": ["mtech", "btech", "phd"],
-    "Sup_id": "prof@mail.com"
+    "Team_name": "Ateam",
+    "Course_name": "Da2020",
+    "Details": "hiihhii",
+    "Admin_id": "prof@mail.com",
+    "Textbook": ["book1", "book2"]
 }
 
-print(insert_acccount(con, row, "INSTRUCTOR", True))
+print(insert_team(con, row, True))
